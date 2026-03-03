270547283300bd3399c64359bcaca4a7b60f73ff
Usani Gideon <125455199+UsaniGee@users.noreply.github.com>
2026-03-03 11:28:45 +0100

    Comment out imageSizes in Media.ts
    Commented out the imageSizes configuration for upload.

--- src/payload/collections/Media.ts
+++ src/payload/collections/Media.ts
@@ -15,26 +15,26 @@ export const Media: CollectionConfig = {
   upload: {
     staticDir: 'media',
     disableLocalStorage: true,
-    imageSizes: [
-      {
-        name: 'thumbnail',
-        width: 400,
-        height: 300,
-        position: 'centre',
-      },
-      {
-        name: 'card',
-        width: 768,
-        height: 1024,
-        position: 'centre',
-      },
-      {
-        name: 'tablet',
-        width: 1024,
-        height: undefined,
-        position: 'centre',
-      },
-    ],
+     // imageSizes: [
+    //   {
+    //     name: 'thumbnail',
+    //     width: 400,
+    //     height: 300,
+    //     position: 'centre',
+    //   },
+    //   {
+    //     name: 'card',
+    //     width: 768,
+    //     height: 1024,
+    //     position: 'centre',
+    //   },
+    //   {
+    //     name: 'tablet',
+    //     width: 1024,
+    //     height: undefined,
+    //     position: 'centre',
+    //   },
+    // ],
     adminThumbnail: 'thumbnail',
     mimeTypes: ['image/*'],
   },
